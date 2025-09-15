6849835c506594d4ea7b7d9818537be282381c75
Kazi Md. Rakibul Hassan <kazrkb@gmail.com>
2025-09-15 14:08:43 +0600

    created a login and welcome page

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
-    return render_template("form.html")
+    return render_template("login.html")
 
 @app.route("/submit", methods=["POST"])
 def submit():
